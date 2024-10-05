c5b192d3bce2a992e2d87b62b19bdfe0fa6e5bc4
jadonburgardt <burgardtjad@gmail.com>
2024-10-05 12:16:57 -0500

    finished

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+# https://realpython.github.io/fake-jobs/
+# https://realpython.com/beautiful-soup-web-scraper-python/#step-3-parse-html-code-with-beautiful-soup
+
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
@@ -17,16 +20,16 @@ results = soup.find(id="ResultsContainer")
 
 job_elements = results.find_all("div", class_="card-content")
 
-for job_element in job_elements:
-  title_element = job_element.find("h2", class_="title")
-  company_element = job_element.find("h3", class_="company")
-  location_element = job_element.find("p", class_="location")
+# for job_element in job_elements:
+#   title_element = job_element.find("h2", class_="title")
+#   company_element = job_element.find("h3", class_="company")
+#   location_element = job_element.find("p", class_="location")
 
-# extract text from HTML elements
-  print(title_element.text.strip())
-  print(company_element.text.strip())
-  print(location_element.text.strip())
-  print()
+#   #extract text from HTML elements
+#   print(title_element.text.strip())
+#   print(company_element.text.strip())
+#   print(location_element.text.strip())
+#   print()
 
 # RESULTS
 ###########################################
@@ -52,3 +55,37 @@ for job_element in job_elements:
 
 # ...
   
+# Find elements by string content
+python_jobs = results.find_all(
+  "h2", string=lambda text: "python" in text.lower()
+)
+
+# access parent elements
+python_job_elements = [
+  h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+# for job_element in python_job_elements:
+#   title_element = job_element.find("h2", class_="title")
+#   company_element = job_element.find("h3", class_="company")
+#   location_element = job_element.find("p", class_="location")
+#   print(title_element.text.strip())
+#   print(company_element.text.strip())
+#   print(location_element.text.strip())
+#   print()
+
+# extract attributes from html elements
+
+for job_element in python_job_elements:
+  title_element = job_element.find("h2", class_="title")
+  company_element = job_element.find("h3", class_="company")
+  location_element = job_element.find("p", class_="location")
+  print(title_element.text.strip())
+  print(company_element.text.strip())
+  print(location_element.text.strip())
+  links = job_element.find_all("a")
+  
+  for link in links:
+    link_url = link["href"]
+    print(f"Apply here: {link_url}")
+  print()
